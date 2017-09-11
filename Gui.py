053ab7acc5a7917cbbd5f700e55349fd28a3f61d
Garrett <skarrj@gmail.com>
2017-09-11 13:08:40 -0600

    Updated Gui
    Updated GUI

--- Gui.py
+++ Gui.py
@@ -153,11 +153,3 @@ if(__name__ == "__main__"):
     # <div class="aui-message aui-message-success"
     # <a class="issue-created-key issue-link" data-issue
 
-    # CONTACTS
-    # [~gary_meyer@partech.com]
-    # [~garrett_breeden@partech.com]
-    # [~steven_eddy@partech.com]
-    # [~Derek_puttmann@partech.com]
-    # [~anglo_espineli@partech.com]
-    # [~chris_wright@partech.com]
-    # [~ryan_schneider@partech.com]
