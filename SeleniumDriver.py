69a808fd507d90e5eecf7dcbcc3e0d260352df3a
Garrett <skarrj@gmail.com>
2017-09-11 21:31:09 -0600

    Update SeleniumDriver.py

--- SeleniumDriver.py
+++ SeleniumDriver.py
@@ -11,13 +11,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 import pyperclip
+import re
 import pickle
 
 
 class SeleniumDriver():
     def __init__(self,):
-        profile = webdriver.FirefoxProfile()
-        self.driver = webdriver.Firefox(profile)
+        self.driver = webdriver.Ie()
         self.sessionCookies = None
 
     def loadPage(self, website, title):
@@ -40,7 +40,7 @@ class SeleniumDriver():
         submit_button = self.driver.find_element_by_xpath(
             ".//*[@id='login-form-submit']")
         submit_button.click()
-        self.saveSessionCookies("InitialLogin.pkl")
+        
 
     def createNewTicket(self):
         title = "Dashboard"
@@ -50,81 +50,86 @@ class SeleniumDriver():
         
         
         self.loadPage(
-            "https://devops.partech.com/jira/secure/CreateIssue!default.jspa", "Create Issue")
+            "https://devops.partech.com/jira/secure/Dashboard.jspa", "System Dashboard")
         #bbs_element = self.driver.find_element_by_xpath(".//*[@id='brink-bugs-and-support-(bbs)-84']/a")
-        self.saveSessionCookies("CreateIssue.pkl")
         
+        create_ticket_button = self.driver.find_element_by_xpath('//*[@id="create_link"]')
+        create_ticket_button.click()
+
+        title = "Create Issue"
+        while((title not in self.driver.title) == True):
+            sleep(.25)
+            print("sleeping .25 seconds to finish loading page") 
 
         project_field_element = self.driver.find_element_by_xpath(
-            ".//*[@id='project-field']")
+            "//*[@id='project-field']")
+        project_field_element.clear()
         project_field_element.send_keys("B3SE")
         project_field_element.send_keys(Keys.ENTER)
 
+        sleep(.5)
         issue_type_field_element = self.driver.find_element_by_xpath(
-            ".//*[@id='issuetype-field']")
+            '//*[@id="issuetype-field"]')
+        issue_type_field_element.clear()
         issue_type_field_element.send_keys("Task")
         issue_type_field_element.send_keys(Keys.ENTER)
+        
 
-        issue_submit_button = self.driver.find_element_by_xpath(
-            ".//*[@id='issue-create-submit']")
-        issue_submit_button.click()
+        # issue_submit_button = self.driver.find_element_by_xpath(
+        #     ".//*[@id='issue-create-submit']")
+        # issue_submit_button.click()
 
     def inputDataToCase(self, summary, description):
-        sleep(2)  # Ensure page loads entirely
-        self.saveSessionCookies("TicketSetup.pkl")
+        sleep(1)
         summary_field = self.driver.find_element_by_xpath(
-            ".//*[@id='summary']")
-        summary_field.click()
+            '//*[@id="summary"]')
         summary_field.send_keys(summary)
 
-        # Select Text Field to Input RAW Markup
-        description_text_field = self.driver.find_element_by_xpath(".//*[@id='aui-uid-1']")
-        description_text_field.click()
         
-        # Input data into description
-        pyperclip.copy(description)
-        summary_field.click()
-        summary_field.send_keys(Keys.TAB, Keys.COMMAND + "v")
+        description_text_field = self.driver.find_element_by_xpath('//*[@id="description"]')
+        description_text_field.click()
+        description_text_field.send_keys(description)
 
         # Set Priority of Case
-        priority_drop_down = self.driver.find_element_by_xpath(".//*[@id='priority-field']")
+        priority_drop_down = self.driver.find_element_by_xpath('//*[@id="priority-field"]')
+        #priority_drop_down.clear()
+        sleep(.5) #Ensure it properly reloads
         priority_drop_down.click()
         priority_drop_down.send_keys("Highest", Keys.TAB)
+
+
 
         # Add state tag
         # tags_drop_down = self.driver.find_element_by_xpath(".//*[@id='labels-multi-select']/span")
         # tags_drop_down.click()
 
-        submit_JIRA_case_button = self.driver.find_element_by_xpath(".//*[@id='issue-create-submit']")
+        submit_JIRA_case_button = self.driver.find_element_by_xpath('//*[@id="create-issue-submit"]')
         submit_JIRA_case_button.click()
 
-        self.driver.quit()
+        JIRA_case_number_element = self.driver.find_element_by_xpath('//*[@id="aui-flag-container"]/div/div')
+        self.JIRA_case_raw = JIRA_case_number_element.get_attribute("innerHTML")
+        self.last_created_JIRA_Case = self.getJIRACase(self.JIRA_case_raw)
+
+
+
+        #self.driver.quit()
 
     # TODO: Finish Parsing Users Function
     def parseUsers(self, users):
         pass
 
-
-        # This is a HTML Select tag, use <option value="10413">Minor</option>
-        # severity_element = self.driver.find_element_by_xpath(
-        #    ".//*[@id='tinymce']/p")
-
         assigned_to_element = self.driver.find_element_by_xpath(
             ".//*[@id='assignee-field']")  
 
     # TODO: Enable postAttachment function
-
     # Let the user control add this until completed
     def postAttachment(self, admin_site, site_id, location_id):
         pass
     
-    def saveSessionCookies(self, name):
-        pickle.dump(self.driver.get_cookies(), open(name, "wb"))
-
-    def loadSessionCookies(self):
-        self.sessionCookies = pickle.load(open("currentSession.pkl", "rb"))
-        for cookie in self.sessionCookies:
-            self.driver.add_cookie(cookie)
+    def getJIRACase(self, searchString):
+        regex = re.search(r'(/jira/browse/B3SE-\d+)', searchString)
+        print(regex.groups()[0])
+        return regex.groups()[0]
 
     
 
@@ -136,10 +141,10 @@ class SeleniumDriver():
 #     firefox.login("")
 #     firefox.createNewTicket()
 #     firefox.inputDataToCase("Sync Error Test",
-#                             "123456-1", "Lets make a new case in JIRA to Showcase useage")
+#                             "5504431-1", "Lets make a new case in JIRA to Showcase useage")
 
     # save new created case
     # .//*[@id='aui-flag-container']/div/div
 
 
-  
+    # [~chris_wright@partech.com] [~angelo_espineli@partech.com] [~garrett_breeden@partech.com]
